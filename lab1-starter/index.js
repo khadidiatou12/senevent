a97107d779ed21dfa12e790994be2d5a36f11454
khadidiatou <khadidiatoudiallo1105@gmail.com>
2026-05-22 09:09:21 +0000

    refactorise le starter en fonctions fléchées

--- lab1-starter/index.js
+++ lab1-starter/index.js
@@ -1,4 +1,4 @@
-"use strict";
+/*"use strict";
 (function () {
   window.addEventListener("load", init);
 
@@ -18,4 +18,21 @@
   function qs(selector) {
     return document.querySelector(selector);
   }
+})();   */
+
+"use strict";
+(function () {
+  const id = name => document.getElementById(name);
+  const qs = selector => document.querySelector(selector);
+
+  const change = () => {
+    qs("p").style.color = "red";
+    qs("h2").style.color = "blue";
+  };
+
+  const init = () => {
+    id("b1").addEventListener("click", change);
+  };
+
+  window.addEventListener("load", init);
 })();
