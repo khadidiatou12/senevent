be4bef7a8c784b84a89e6119489c7140e8fae3af
Sokhna Fatou Diop <sokhnafatoudiop329@gmail.com>
2026-06-04 21:03:14 +0000

    refactore le starter en fonctions flechees

--- lab1-starter/index.js
+++ lab1-starter/index.js
@@ -1,17 +1,13 @@
 "use strict";
 (function () {
-  window.addEventListener("load", init);
-  function init() {
-    id("b1").addEventListener("click", change);
-  }
-  function change() {
+  const id = name => document.getElementById(name);
+  const qs = selector => document.querySelector(selector);
+  const change = () => {
     qs("p").style.color = "red";
     qs("h2").style.color = "blue";
-  }
-  function id(name) {
-    return document.getElementById(name);
-  }
-  function qs(selector) {
-    return document.querySelector(selector);
-  }
+  };
+  const init = () => {
+    id("b1").addEventListener("click", change);
+  };
+  window.addEventListener("load", init);
 })();
